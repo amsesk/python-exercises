089562767653186329b0a606e7b8cf7eab7a83fe
amsesk <amsesk@umich.edu>
2021-01-10 16:16:41 -0500

    exercise2 initial

---
+++ exercise_2/README.md
@@ -0,0 +1,30 @@
+# Exercise 2
+
+### Setup
+
+A student wrote a script (*table_reader.py*) to read the 3-column data in *exercise2_table.tsv*, sum the values in the second and third columns, and print the results 2-column data table to the console. Since they were learning about python objects at the time, they decided to define two classes (TableReader and TableRow) to accomplish this task. They have made several errors in their script that are precluding it from running and/or from producing the intended result (below). Fix their errors so that your output matches that below.
+
+*Hint*: `TableReader` is intended to handle the entire table while `TableRow` is intended to handle individual rows in the table.
+
+*Hint*: Don't hessitate to use "print line debugging", where you print out intermediate values generated while the script is running by adding `print` statements around where you are getting errors in execution.
+ 
+### Intended Output
+
+```
+Sample_Id_1	Sum_col2_col3
+1	114
+2	86
+3	21
+4	75
+5	117
+6	69
+7	76
+8	56
+9	2
+10	30
+11	82
+12	102
+13	86
+14	93
+15	62
+```
---
+++ exercise_2/table_reader.py
@@ -0,0 +1,67 @@
+class TableRow(object):
+
+    def __init__(self, row_text, delimiter):
+        
+        self.row_text = row_text
+
+        self.delimiter = delimiter
+
+        self.values = self.parse_row_into_columns(self.row_text)
+
+    def parse_row_into_columns(self, row_text):
+
+        stripped = row_text.strip()
+
+        spl = stripped.split(self.delimiter)
+
+        return spl
+
+class TableReader(object):
+
+    def __init__(self, path_to_file, delimiter):
+
+        self.table_file_object = open(path_to_file, 'r')
+
+        self.delimiter = delimiter
+
+        self.rows = []
+
+    def read(self):
+
+        # First row contains headers, so skip it
+        first_line = self.table_file_object.readline()
+
+        for line in self.table_file_object.readlines():
+
+            this_row = TableRow(row_text = line, delimiter = self.delimiter)
+
+            self.rows.append(this_row)
+
+    def sum_second_and_third_columns(self):
+
+        print ("Sample_Id_1\tSum_col2_col3")
+        for row in self.rows:
+            
+            this_sample = row.values[0]
+            this_sum = row.values[1] + row.values[2]
+
+            print(f"{this_sample}\t{this_sum}")
+
+    def close_file_object(self):
+        self.table_file_object.close()
+
+
+if __name__ == "__main__":
+
+    my_table_path = "exercise2_table.tsv"
+
+    my_table_reader = TableReader(my_table_path, delimiter = ",")
+
+    my_table_reader.sum_second_and_third_columns()
+
+    my_table_reader.close_file_object()
+
+    
+
+
+
